b360e85dda054d5cd503de98bacf9faa7c2f7a75
Maxime Baumann <maxime.baumann.pro@gmail.com>
2019-12-09 22:10:29 +0100

    Begin Day3

---
+++ src/MBaumann.CodingGames.AdventOfCode2019/Days/Day3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MBaumann.CodingGames.AdventOfCode2019.Days
+{
+    public sealed class Day3
+    {
+        public int FirstPart()
+        {
+            string[] v_RawInput = File.ReadAllLines($"Inputs{Path.DirectorySeparatorChar}Day3.txt");
+
+            return ComputeDistance(v_RawInput[0], v_RawInput[1]);
+        }
+
+        public static int ComputeDistance(string p_FirstPath, string p_SecondPath)
+        {
+            return 0;
+        }
+    }
+}
---
+++ test/MBaumann.CodingGames.AdventOfCode2019.Tests/Days/Day3Tests.cs
@@ -0,0 +1,17 @@
+﻿using MBaumann.CodingGames.AdventOfCode2019.Days;
+using Xunit;
+
+namespace MBaumann.CodingGames.AdventOfCode2019.Tests.Days
+{
+    public sealed class Day3Tests
+    {
+        [Theory]
+        [InlineData("R8,U5,L5,D3", "U7,R6,D4,L4", 6)]
+        [InlineData("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 159)]
+        [InlineData("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135)]
+        public void Part1Tests(string p_FirstPath, string p_SecondPath, int p_Distance)
+        {
+            Assert.Equal(p_Distance, Day3.ComputeDistance(p_FirstPath, p_SecondPath));
+        }
+    }
+}
