138f68149f1f8837a9c801bbc68174914f720d84
Maxime Baumann <maxime.baumann.pro@gmail.com>
2020-12-05 20:21:04 +0100

    AOC2020D04P2

--- src/MBaumann.CodingGames.AdventOfCode2020/Days/Day4.cs
+++ src/MBaumann.CodingGames.AdventOfCode2020/Days/Day4.cs
@@ -3,24 +3,40 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace MBaumann.CodingGames.AdventOfCode2020.Days
 {
-    class Day4
+    internal static class Day4
     {
         public static int FirstPart()
         {
             return ValidPassportsCount(File.ReadAllText($"Inputs{Path.DirectorySeparatorChar}Day4.txt"));
+        }
+        
+        public static int SecondPart()
+        {
+            return ValidPassportsCountPart2(File.ReadAllText($"Inputs{Path.DirectorySeparatorChar}Day4.txt"));
         }
         public static int ValidPassportsCount(string p_Input)
         {
             return p_Input.Split(new [] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).Count(IsValid);
         }
 
+        public static int ValidPassportsCountPart2(string p_Input)
+        {
+            return p_Input.Split(new [] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).Count(IsValidPart2);
+        }
+
+        private static readonly IReadOnlyCollection<string> EYES_COLORS = new[] {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+        private static readonly Regex HEIGHT_REGEX = new Regex(@"^(?<inches>\d{2})in$|^(?<centimeters>\d{3})cm$", RegexOptions.Compiled | RegexOptions.ECMAScript);
+        private static readonly Regex HAIR_COLOR_REGEX = new Regex(@"^#[0-9a-f]{6}$", RegexOptions.Compiled | RegexOptions.ECMAScript);
+        private static readonly Regex PID_REGEX = new Regex(@"^[0-9]{9}$", RegexOptions.Compiled | RegexOptions.ECMAScript);
+
         private static bool IsValid(string p_Passport)
         {
             var v_Fields = p_Passport.Split(new[] {'\n', ' '}, StringSplitOptions.RemoveEmptyEntries);
-            
+
             if (!v_Fields.Any(f => f.StartsWith("byr")))
                 return false;
             if (!v_Fields.Any(f => f.StartsWith("iyr")))
@@ -35,6 +51,62 @@ namespace MBaumann.CodingGames.AdventOfCode2020.Days
                 return false;
             if (!v_Fields.Any(f => f.StartsWith("pid")))
                 return false;
+
+            return true;
+        }
+
+        internal static bool IsValidPart2(string p_Passport)
+        {
+            var v_Fields = p_Passport.Split(new[] {'\n', '\r', ' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            try
+            {
+                var v_Byr = int.Parse(v_Fields.First(f => f.StartsWith("byr:")).Substring(4));
+                if (v_Byr is < 1920 or > 2002)
+                    return false;
+
+                var v_Iyr = int.Parse(v_Fields.First(f => f.StartsWith("iyr:")).Substring(4));
+                if (v_Iyr is < 2010 or > 2020)
+                    return false;
+
+                var v_Eyr = int.Parse(v_Fields.First(f => f.StartsWith("eyr:")).Substring(4));
+                if (v_Eyr is < 2020 or > 2030)
+                    return false;
+
+                var v_Hgt = v_Fields.First(f => f.StartsWith("hgt:")).Substring(4);
+                var v_HgtMatch = HEIGHT_REGEX.Match(v_Hgt);
+                if (!v_HgtMatch.Success)
+                    return false;
+
+                var v_Centimeters = v_HgtMatch.Groups["centimeters"];
+                var v_Inches = v_HgtMatch.Groups["inches"];
+
+                if (v_Centimeters.Success && int.Parse(v_Centimeters.Value) is < 150 or > 193)
+                    return false;
+
+                if (v_Inches.Success && int.Parse(v_Inches.Value) is < 59 or > 76)
+                    return false;
+
+                if (!v_Centimeters.Success && !v_Inches.Success)
+                    return false;
+                
+                var v_Hcl = v_Fields.First(f => f.StartsWith("hcl:")).Substring(4);
+                if (!HAIR_COLOR_REGEX.IsMatch(v_Hcl))
+                    return false;
+
+                var v_Ecl = v_Fields.First(f => f.StartsWith("ecl:")).Substring(4);
+                if (!EYES_COLORS.Contains(v_Ecl))
+                    return false;
+
+                var v_Pid = v_Fields.First(f => f.StartsWith("pid:")).Substring(4);
+                if (!PID_REGEX.IsMatch(v_Pid))
+                    return false;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
 
             return true;
         }
--- test/MBaumann.CodingGames.AdventOfCode2020.Tests/Days/Day4Tests.cs
+++ test/MBaumann.CodingGames.AdventOfCode2020.Tests/Days/Day4Tests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MBaumann.CodingGames.AdventOfCode2020.Days;
+using Newtonsoft.Json;
 using NFluent;
 using Xunit;
 
@@ -25,10 +27,108 @@ hgt:179cm
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in";
 
+        private const string INPUT_INVALID = @"eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007";
+        
+        private const string INPUT_VALID = @"pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719";
+
         [Fact]
         public void Part1()
         {
             Check.That(Day4.ValidPassportsCount(INPUT)).IsEqualTo(2);
+        }
+        
+        [Fact]
+        public void Part2a()
+        {
+            Check.That(Day4.ValidPassportsCountPart2(INPUT_INVALID)).IsEqualTo(0);
+        }
+        
+        [Fact]
+        public void Part2b()
+        {
+            Check.That(Day4.ValidPassportsCountPart2(INPUT_VALID)).IsEqualTo(4);
+        }
+
+        [Fact]
+        public void Day4ShouldWork()
+        {
+            Check.That(Day4.ValidPassportsCountPart2(File.ReadAllText($"Inputs{Path.DirectorySeparatorChar}Day4.txt")))
+                .IsEqualTo(131);
+        }
+
+        [Theory]
+        [InlineData("byr:1988 hgt:160cm eyr:2023 hcl:#866857 pid:788805179 iyr:2022 ecl:amb", false)]
+        [InlineData("hgt:164cm eyr:2023 byr:2008 ecl:grn pid:420168481 hcl:#b6652a iyr:2012", false)]
+        [InlineData(@"ecl:hzl eyr:2031 cid:145 hgt:186cm hcl:#cfa07d
+byr:1941 iyr:2010 pid:722056139", false)]
+        public void PassportMustMatchExpected(string p_Passport, bool p_Expected)
+        {
+            Check.That(Day4.IsValidPart2(p_Passport)).IsEqualTo(p_Expected);
+        }
+
+        [Fact]
+        public void Day4Debug()
+        {
+            var v_Input = File.ReadAllText($"Inputs{Path.DirectorySeparatorChar}Day4.txt")
+                .Split(new[] {"\n\n"}, StringSplitOptions.RemoveEmptyEntries);
+            var v_JsonCheck =
+                JsonConvert.DeserializeObject<JsonCheck>(
+                    File.ReadAllText($"Files{Path.DirectorySeparatorChar}day4.json"));
+
+            var v_Counter = 0;
+
+            foreach (var v_Passport in v_Input)
+            {
+                var v_IsValid = Day4.IsValidPart2(v_Passport);
+                var v_Check = v_JsonCheck.Input[v_Counter];
+                var v_Expected = v_Check.IsValid;
+
+                if (v_IsValid != v_Expected)
+                {
+                    Day4.IsValidPart2(v_Passport);
+                }
+
+                v_Counter++;
+            }
+        }
+
+        public class JsonCheck
+        {
+            public Input[] Input { get; set; }
+
+            public int Count { get; set; }
+        }
+
+        public class Input
+        {
+            public string V { get; set; }
+
+            public bool IsValid { get; set; }
+
+            public string Check { get; set; }
         }
     }
 }
